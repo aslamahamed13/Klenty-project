7c6915abb4db4c124cf24ac6a5038c504f128e24
aslamahamed13 <aslamahamed13@gmail.com>
2022-02-09 22:36:37 +0530

    klenty

---
+++ html/script.js
@@ -0,0 +1,10 @@
+let slider = tns({
+    controlsContainer: '#controls',
+    prevButton: '.previous',
+    nextButton: '.next',
+    container: '.my-slider',
+    items: 1,
+    slideBy: 'page',
+  });
+
+  
